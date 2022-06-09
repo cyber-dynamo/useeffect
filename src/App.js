d7315938328a49c54855a1d344d4316cb8fe301f
Paco Chan <pacochan@Pacos-MacBook-Air.local>
2022-06-08 22:38:06 -0700

    k

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [starWarsData,setStarWarsData]=React.useState({})
+  const [count,setCount]=React.useState(1)
+  React.useEffect(function(){
+    fetch(`https://swapi.dev/api/people/${count}`)
+    .then(res =>res.json())
+    .then(data=>setStarWarsData(data))
+  },[count])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <pre>{JSON.stringify(starWarsData,null,2)}</pre>
+      <button onClick={()=>setCount(prevCount=>prevCount+1)}>Get Next Character</button>
     </div>
   );
 }
